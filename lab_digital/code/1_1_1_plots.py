199e2d31930d6add4e3f8a0a71e39d78512eedd8
Kevin Yu <thekevinyu@gmail.com>
2014-02-18 18:16:30 -0800

    Attempt at better subplotting on 1.1.1 and added 1.1.2

--- lab_digital/code/1_1_1_plots.py
+++ lab_digital/code/1_1_1_plots.py
@@ -1,16 +1,30 @@
 from pylab import *
 import numpy as np
 
+from dft import dft
 
+exclude = ()
 t = np.arange(1e-5, 257e-5, 1e-5)
+
+
+f, subplotaxes = plt.subplots(9, 2, sharex="col", sharey=False)
 for x in range(1, 10):
+    if x in exclude:
+        continue
     filedict = np.load("../data/1_1_1/fsig_%s.0.npz" % (x*1000))
     data = filedict["arr_0"]
-    subplot(9, 2, x * 2 - 1)
-    title("%s Hz" % (x * 1000))
-    plot(t, data)
-    
-    data_fft = np.fft.fft(data)
-    subplot(9, 2, (x * 2))
-    title("%s Hz" % (x * 1000))
-    plot(np.fft.fftfreq(256) * 10e3, data_fft)
+    ax = subplotaxes[x-1][0]
+    ax.set_title(r"$\nu_{sig}=%s Hz$" % (x * 1000))
+    ax.plot(t, data)
+    ax.set_xlim(0, 0.0005)
+
+    f_sample = 10e3
+    freq, data_fourier = dft(data, f_sample=f_sample, power=True)
+    ax = subplotaxes[x-1][1]
+    ax.set_title(r"$\nu_{sig}=%s Hz$" % (x * 1000))
+    ax.plot(freq, data_fourier)
+
+# Fine-tune figure; make subplots close to each other and hide x ticks for
+# all but bottom plot.
+# f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
--- lab_digital/code/1_1_2_plots.py
+++ lab_digital/code/1_1_2_plots.py
@@ -1,4 +1,33 @@
 from pylab import *
 import numpy as np
 
+from dft import dft
 
+
+t = np.arange(1e-5, 257e-5, 1e-5)
+
+# f_signal = f_nu
+filedict = np.load("../data/1_1_2/f_10000Hz.npz")
+data = filedict["arr_0"]
+title(r"$\nu_{sig}=\nu_{samp}=10 kHz$")
+plot(t, data)
+xlim(0, 0.0005)
+ylim(-1, 1)
+
+figure()
+# f_signal >> f_sample
+subplot(211)
+filedict = np.load("../data/1_1_2/fast_signal.npz")
+data = filedict["arr_0"]
+title(r"$\nu_{sig}=10MHz,\nu_{samp}=7kHz$")
+plot(t, data)
+
+# TODO plot an acutal 10MHz wave on top of this
+
+subplot(212)
+f_sample = 7e3
+freq, data_fourier = dft(data, f_sample=f_sample, power=True, f_max=10e3, f_res=1.)
+title(r"$\nu_{sig}=10MHz,\nu_{samp}=7kHz$")
+plot(freq, data_fourier)
+
+# TODO plot actual foureir transform of 10MHZ sine wave on top of this
