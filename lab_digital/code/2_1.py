0aea27525ea5e03b0fbf304867849b1b8d59f8e9
Kevin Yu <thekevinyu@gmail.com>
2014-02-20 18:31:08 -0800

    updated data for 2.1.1

--- lab_digital/code/2_1.py
+++ lab_digital/code/2_1.py
@@ -5,20 +5,20 @@ import DFEC
 
 
 f_sample = 10e6  # 10 MHz
-f_lo = 100e3  # 100 kHz
-f_signal_p = 105e3  # 105 kHz
-f_signal_m = 95e3  # 95 kHz
+f_lo = 800e3  # 200 kHz
+f_signal_p = 840e3  # 210 kHz
+f_signal_m = 760e3  # 190 kHz
 
 
-N = 256
+N = 16384
 yes_array = ["y", "Y"]
 
 for f_sig in [f_signal_p, f_signal_m]:
     go = raw_input("Safe to set srs?\n")
     if go not in yes_array:
         break
-    DFEC.set_srs(1, freq=f_sig, vpp=0.6)
-    DFEC.set_srs(2, freq=f_lo, vpp=0.6)
+    DFEC.set_srs(1, freq=f_sig, vpp=0.5)
+    DFEC.set_srs(2, freq=f_lo, vpp=0.5)
     go = raw_input("Check that SRS1 is outputting f=%s Hz\n and SRS2 is outputting f=%s Hz\n" % (
         f_sig, f_lo))
     if go not in yes_array:
